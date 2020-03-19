88aa3fba12d495ae29f89b6ca15e539f1b3de95a
Matteo <matzar1808@gmail.com>
2020-03-19 15:06:35 +0100

    Error messages

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,18 @@ use std::env;
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
-
+     let mut contents = String::new();
     for arg in &args[1..] {
-        let mut contents = String::new();
+        let mut _fichier = match File::open(&arg){
+            Ok(_f) => {       
         let mut _file = File::open(&arg)?.read_to_string(&mut contents)?;
         println!("{}", contents);
-    }
+            }
+            Err(_e) => {
+                println!("Le fichier n'existe pas")
+            }
+        };
+
+}
     Ok(())
 }
