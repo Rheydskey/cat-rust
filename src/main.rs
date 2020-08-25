a5320ca724613cb778c411c85b0db96f76aa5384
Rheydskey <matgag02@gmail.com>
2020-08-25 23:20:10 +0200

    Update the functioning of program

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
      let mut contents = String::new();
     for arg in &args[1..] {
-        let mut _fichier = match File::open(&arg){
-            Ok(_f) => {       
-        let mut _file = File::open(&arg).expect("The file is not found").read_to_string(&mut contents).expect("I can't read the file");
-        println!("{}", contents);
-            }
-            Err(_e) => {
-                println!("Le fichier n'existe pas")
-            }
-        };
-
+       File::open(&arg).expect("Le fichier n'existe pas").read_to_string(&mut contents).expect("Impossible de lire");
+       println!("{}", contents);
 }
 }
