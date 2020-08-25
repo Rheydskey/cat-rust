3aa8bbdbfcb5a24e2c55d4d0dc714a30ff8f54a6
Rheydskey <matgag02@gmail.com>
2020-08-25 23:15:12 +0200

    Update Expect

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     for arg in &args[1..] {
         let mut _fichier = match File::open(&arg){
             Ok(_f) => {       
-        let mut _file = File::open(&arg).expect("Error").read_to_string(&mut contents).expect("Error");
+        let mut _file = File::open(&arg).expect("The file is not found").read_to_string(&mut contents).expect("I can't read the file");
         println!("{}", contents);
             }
             Err(_e) => {
