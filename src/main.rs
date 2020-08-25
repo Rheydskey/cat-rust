41328b54e223d4378b92c8c96704718be3afcb01
Rheydskey <matgag02@gmail.com>
2020-08-25 23:14:29 +0200

    remove Result and add expect

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::env;
 
-fn main() -> std::io::Result<()> {
+fn main() {
     let args: Vec<String> = env::args().collect();
      let mut contents = String::new();
     for arg in &args[1..] {
         let mut _fichier = match File::open(&arg){
             Ok(_f) => {       
-        let mut _file = File::open(&arg)?.read_to_string(&mut contents)?;
+        let mut _file = File::open(&arg).expect("Error").read_to_string(&mut contents).expect("Error");
         println!("{}", contents);
             }
             Err(_e) => {
@@ -17,5 +17,4 @@ fn main() -> std::io::Result<()> {
         };
 
 }
-    Ok(())
 }
