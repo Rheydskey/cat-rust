bb1a97ed805e5f34b2fa8bd60cd1db8f063ea0d8
Rheydskey <matgag02@gmail.com>
2021-03-15 17:51:43 +0100

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use std::fs::File;
-use std::io::prelude::*;
-use std::env;
+use std::io::Read;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-     let mut contents = String::new();
-    for arg in &args[1..] {
-       File::open(&arg).expect("Le fichier n'existe pas").read_to_string(&mut contents).expect("Impossible de lire");
-       println!("{}", contents);
-}
+    let mut contents = String::new();
+    for arg in &std::env::args().collect::<Vec<String>>()[1..] {
+        std::fs::File::open(&arg)
+            .expect("Le fichier n'existe pas")
+            .read_to_string(&mut contents)
+            .expect("Impossible de lire");
+        println!("{}", contents);
+    }
 }
